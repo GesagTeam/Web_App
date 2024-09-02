33f9b1996ac87ab46879eeac604026bff2e4a217
Mohaimen Rady <radyradymohaimen@gmail.com>
2024-09-02 12:08:33 +0200

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Container} from "react-bootstrap";
+import Signup from "./components/Signup";
+import ForgetPassword from "./components/ForgetPassword";
+import UpdateProfile from "./components/UpdateProfile";
+import Dashboard from "./components/DashBoard"
+import Signin from "./components/Signin"
+import AuthProvider from "./context/AuthContext";
+import RequireAuth from "./context/RequireAuth";
 
+const App = () => {
 
-class App extends Component{
-      constructor(){
-          super()
-
-      }
-
-render(){
-      return(
-
-        <div>
-
-          </div>
-      );
-
-
-}
-
-
-}
-
-export default App;
+      return (
+            <Container
+              className="d-flex align-items-center justify-content-center"
+              style={{ minHeight: "100vh" }}
+            >
+              <div className="w-100" style={{ maxWidth: "400px" }}>
+                <Router>
+                  <AuthProvider>
+                    <Routes>
+                      <Route
+                        path="/"
+                        element={
+                          <RequireAuth>
+                            <Dashboard />
+                          </RequireAuth>
+                        }
+                      />
+                      <Route path="/update-profile" element={<UpdateProfile />} />
+                      <Route path="/signup" element={<Signup />} />
+                      <Route path="/login" element={<Signin />} />
+                      <Route path="/forgot-password" element={<ForgetPassword />} />
+                    </Routes>
+                  </AuthProvider>
+                </Router>
+              </div>
+            </Container>
+          );
+        };
+        
+        export default App;
+        
